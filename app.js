45040ef3c52c70388b12f9877d8d382881b17e36
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-01-31 21:46:21 -0500

    render barbell

--- app.js
+++ app.js
@@ -56,8 +56,6 @@ const model = {
     }
     this.platesAmt[weight]['included'] = include;
     this.calcPlates();
-    console.log(weight);
-    console.log(include);
   },
   calcPlates: function () {
     const weights = [45, 35, 25, 10, 5, 2.5];
@@ -81,6 +79,80 @@ const view = {
   },
   renderTargetWeightLoad: function (platesAmt) {
     document.querySelector('#target-load').value = 45 + 2 * (platesAmt[45]['amt'] * 45 + platesAmt[35]['amt'] * 35 + platesAmt[25]['amt'] * 25 + platesAmt[10]['amt'] * 10 + platesAmt[5]['amt'] * 5 + platesAmt['2.5']['amt'] * 2.5);
+  },
+  renderBarbell: function (platesAmt) {
+    const canvas = document.getElementById("barbell");
+    const ctx = canvas.getContext("2d");
+    // Plate dimensions
+    const plateMargin = 20;
+
+    const plateWidth = 15;
+    const plateHeight = 75;
+
+    const plateWidth35 = 15;
+    const plateHeight35 = 65;
+
+    const plateWidth25 = 15;
+    const plateHeight25 = 50;
+
+    const plateWidth10 = 15;
+    const plateHeight10 = 40;
+
+    const plateWidth5 = 10;
+    const plateHeight5 = 30;
+
+    const plateWidth2_5 = 7.5;
+    const plateHeight2_5 = 20;
+
+    // Barbell dimensions
+    const barbellWidth = canvas.width;
+    const barbellHeight = 10;
+    const barbellMargin = 40;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw the barbell
+    ctx.fillRect(0, (canvas.height - barbellHeight) / 2, barbellWidth, barbellHeight);
+
+    // Draw the 45 lb plates
+    for (let i = 0; i < platesAmt[45]['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight) / 2, plateWidth, plateHeight);
+      ctx.fillRect(barbellWidth / 2 + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight) / 2, plateWidth, plateHeight);
+    }
+
+    // Draw the 35 lb plates
+    for (let i = 0; i < platesAmt[35]['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (platesAmt[45]['amt'] * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight35) / 2, plateWidth35, plateHeight35);
+      ctx.fillRect(barbellWidth / 2 + (platesAmt[45]['amt'] * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight35) / 2, plateWidth35, plateHeight35);
+    }
+
+    // Draw the 25 lb plates
+    for (let i = 0; i < platesAmt[25]['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (platesAmt[45]['amt'] * plateMargin) - (platesAmt[35]['amt'] * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight25) / 2, plateWidth25, plateHeight25);
+      ctx.fillRect(barbellWidth / 2 + (platesAmt[45]['amt'] * plateMargin) + (platesAmt[35]['amt'] * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight25) / 2, plateWidth25, plateHeight25);
+    }
+
+    // Draw the 10 lb plates
+    for (let i = 0; i < platesAmt[10]['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (platesAmt[45]['amt'] * plateMargin) - (platesAmt[35]['amt'] * plateMargin) - (platesAmt[25]['amt'] * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight10) / 2, plateWidth10, plateHeight10);
+      ctx.fillRect(barbellWidth / 2 + (platesAmt[45]['amt'] * plateMargin) + (platesAmt[35]['amt'] * plateMargin) + (platesAmt[25]['amt'] * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight10) / 2, plateWidth10, plateHeight10);
+    }
+
+    // Draw the 5 lb plates
+    for (let i = 0; i < platesAmt[5]['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (platesAmt[45]['amt'] * plateMargin) - (platesAmt[35]['amt'] * plateMargin) - (platesAmt[25]['amt'] * plateMargin) - (platesAmt[10]['amt'] * plateMargin) - (i * plateMargin / 1.5) - barbellMargin + plateWidth5 / 2, (canvas.height - plateHeight5) / 2, plateWidth5, plateHeight5);
+      ctx.fillRect(barbellWidth / 2 + (platesAmt[45]['amt'] * plateMargin) + (platesAmt[35]['amt'] * plateMargin) + (platesAmt[25]['amt'] * plateMargin) + (platesAmt[10]['amt'] * plateMargin) + (i * plateMargin / 1.5) + (barbellMargin - plateWidth), (canvas.height - plateHeight5) / 2, plateWidth5, plateHeight5);
+    }
+
+    // Draw the 2.5 lb plates
+    for (let i = 0; i < platesAmt['2.5']['amt']; i++) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(barbellWidth / 2 - (platesAmt[45] * plateMargin) - (platesAmt[35] * plateMargin) - (platesAmt[25] * plateMargin) - (platesAmt[10] * plateMargin) - (platesAmt[5] * plateMargin) - (i * plateMargin / 1.5) - barbellMargin + plateWidth2_5 * 1.25, (canvas.height - plateHeight2_5) / 2, plateWidth2_5, plateHeight2_5);
+      ctx.fillRect(barbellWidth / 2 + (platesAmt[45] * plateMargin) + (platesAmt[35] * plateMargin) + (platesAmt[25] * plateMargin) + (platesAmt[10] * plateMargin) + (platesAmt[5] * plateMargin) + (i * plateMargin / 1.5) + (barbellMargin - plateWidth) - plateWidth2_5 * .25, (canvas.height - plateHeight2_5) / 2, plateWidth2_5, plateHeight2_5);
+    }
   }
 
 }
@@ -89,6 +161,7 @@ const controller = {
   updateTargetLoad: function (updatedTargetLoad) {
     model.updateTargetLoad(updatedTargetLoad);
     view.renderPlatesPerSide(model.platesAmt);
+    view.renderBarbell(model.platesAmt);
   },
   handleTargetLoadInput: function (e) {
     this.updateTargetLoad(e.target.value);
@@ -97,16 +170,19 @@ const controller = {
     model.decrementPlatesAmt(e.target.classList[1].slice(7));
     view.renderPlatesPerSide(model.platesAmt);
     view.renderTargetWeightLoad(model.platesAmt);
+    view.renderBarbell(model.platesAmt);
   },
   incrementValue: (e) => {
     model.incrementPlatesAmt(e.target.classList[1].slice(7));
     view.renderPlatesPerSide(model.platesAmt);
     view.renderTargetWeightLoad(model.platesAmt);
+    view.renderBarbell(model.platesAmt);
   },
   updateIncluded: function (updatedWeightIncluded) {
     model.updateIncluded(updatedWeightIncluded.id.slice(8), updatedWeightIncluded.checked);
     view.renderPlatesPerSide(model.platesAmt);
     view.renderTargetWeightLoad(model.platesAmt);
+    view.renderBarbell(model.platesAmt);
   },
   handleIncludeInput: function (e) {
     this.updateIncluded(e.target);
@@ -142,92 +218,3 @@ document.querySelectorAll('input[type="checkbox"]').forEach(function (check) {
     controller.handleIncludeInput(e);
   });
 });
-
-// Get the canvas element
-const canvas = document.getElementById("barbell");
-const ctx = canvas.getContext("2d");
-
-// Get the counter values for each row
-let weight45Counter = 0;
-let weight35Counter = 0;
-let weight25Counter = 0;
-let weight10Counter = 0;
-let weight5Counter = 0;
-let weight2_5Counter = 0;
-
-// Plate dimensions
-const plateMargin = 20;
-
-const plateWidth = 15;
-const plateHeight = 75;
-
-const plateWidth35 = 15;
-const plateHeight35 = 65;
-
-const plateWidth25 = 15;
-const plateHeight25 = 50;
-
-const plateWidth10 = 15;
-const plateHeight10 = 40;
-
-const plateWidth5 = 10;
-const plateHeight5 = 30;
-
-const plateWidth2_5 = 7.5;
-const plateHeight2_5 = 20;
-
-// Barbell dimensions
-const barbellWidth = canvas.width;
-const barbellHeight = 10;
-const barbellMargin = 40;
-
-// Render Barbell & Plates
-function render() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // Draw the barbell
-  ctx.fillRect(0, (canvas.height - barbellHeight) / 2, barbellWidth, barbellHeight);
-
-  // Draw the 45 lb plates
-  for (let i = 0; i < weight45Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight) / 2, plateWidth, plateHeight);
-    ctx.fillRect(barbellWidth / 2 + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight) / 2, plateWidth, plateHeight);
-  }
-
-  // Draw the 35 lb plates
-  for (let i = 0; i < weight35Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (weight45Counter * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight35) / 2, plateWidth35, plateHeight35);
-    ctx.fillRect(barbellWidth / 2 + (weight45Counter * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight35) / 2, plateWidth35, plateHeight35);
-  }
-
-  // Draw the 25 lb plates
-  for (let i = 0; i < weight25Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (weight45Counter * plateMargin) - (weight35Counter * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight25) / 2, plateWidth25, plateHeight25);
-    ctx.fillRect(barbellWidth / 2 + (weight45Counter * plateMargin) + (weight35Counter * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight25) / 2, plateWidth25, plateHeight25);
-  }
-
-  // Draw the 10 lb plates
-  for (let i = 0; i < weight10Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (weight45Counter * plateMargin) - (weight35Counter * plateMargin) - (weight25Counter * plateMargin) - (i * plateMargin) - barbellMargin, (canvas.height - plateHeight10) / 2, plateWidth10, plateHeight10);
-    ctx.fillRect(barbellWidth / 2 + (weight45Counter * plateMargin) + (weight35Counter * plateMargin) + (weight25Counter * plateMargin) + (i * plateMargin) + (barbellMargin - plateWidth), (canvas.height - plateHeight10) / 2, plateWidth10, plateHeight10);
-  }
-
-  // Draw the 5 lb plates
-  for (let i = 0; i < weight5Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (weight45Counter * plateMargin) - (weight35Counter * plateMargin) - (weight25Counter * plateMargin) - (weight10Counter * plateMargin) - (i * plateMargin / 1.5) - barbellMargin + plateWidth5 / 2, (canvas.height - plateHeight5) / 2, plateWidth5, plateHeight5);
-    ctx.fillRect(barbellWidth / 2 + (weight45Counter * plateMargin) + (weight35Counter * plateMargin) + (weight25Counter * plateMargin) + (weight10Counter * plateMargin) + (i * plateMargin / 1.5) + (barbellMargin - plateWidth), (canvas.height - plateHeight5) / 2, plateWidth5, plateHeight5);
-  }
-
-  // Draw the 2.5 lb plates
-  for (let i = 0; i < weight2_5Counter; i++) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(barbellWidth / 2 - (weight45Counter * plateMargin) - (weight35Counter * plateMargin) - (weight25Counter * plateMargin) - (weight10Counter * plateMargin) - (weight5Counter * plateMargin) - (i * plateMargin / 1.5) - barbellMargin + plateWidth2_5 * 1.25, (canvas.height - plateHeight2_5) / 2, plateWidth2_5, plateHeight2_5);
-    ctx.fillRect(barbellWidth / 2 + (weight45Counter * plateMargin) + (weight35Counter * plateMargin) + (weight25Counter * plateMargin) + (weight10Counter * plateMargin) + (weight5Counter * plateMargin) + (i * plateMargin / 1.5) + (barbellMargin - plateWidth) - plateWidth2_5 * .25, (canvas.height - plateHeight2_5) / 2, plateWidth2_5, plateHeight2_5);
-  }
-}
-
-render();
